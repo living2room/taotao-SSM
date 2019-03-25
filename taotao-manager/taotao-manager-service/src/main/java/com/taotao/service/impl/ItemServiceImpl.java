bcb8624f2a7b63c5a5b433f861d131918d0aa1fc
living2room <living2room@sina.com>
2019-03-25 22:44:22 +0800

    5555

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -12,28 +12,42 @@ import com.taotao.common.pojo.EUDataGridResult;
 import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
 import com.taotao.pojo.TbItemExample;
+import com.taotao.pojo.TbItemExample.Criteria;
 import com.taotao.service.ItemService;
 
 @Service
-public class ItemServiceImpl implements ItemService{
-	
-	@Autowired(required=false)
-	@Qualifier("itemmapper")
-	private TbItemMapper itemmapper;
-	
+public class ItemServiceImpl implements ItemService {
+
+	@Autowired(required = false)
+	@Qualifier("itemMapper")
+	private TbItemMapper itemMapper;
+
 	@Override
 	public EUDataGridResult getItemList(int page, int rows) {
 		TbItemExample example = new TbItemExample();
-		
+
 		PageHelper.startPage(page, rows);
-		
-		List<TbItem> Ilist = itemmapper.selectByExample(example);
+
+		List<TbItem> Ilist = itemMapper.selectByExample(example);
 
 		EUDataGridResult result = new EUDataGridResult();
 		result.setRows(Ilist);
 		PageInfo<TbItem> info = new PageInfo<TbItem>(Ilist);
 		result.setTotal(info.getTotal());
-		return result; 
+		return result;
+	}
+
+	@Override
+	public TbItem getItemById(Long itemId) {
+		TbItemExample example = new TbItemExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andIdEqualTo(itemId);
+		List<TbItem> list = itemMapper.selectByExample(example);
+		if (list != null && list.size() > 0) {
+			TbItem item = list.get(0);
+			return item;
+		}
+		return null;
 	}
 
 }
--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -3,10 +3,12 @@ package com.taotao.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.taotao.common.pojo.EUDataGridResult;
+import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
 	
 @Controller("itemController")
@@ -16,7 +18,13 @@ public class ItemController {
 	@Qualifier("itemService")
 	private ItemService itemService;
 
-
+	@RequestMapping("/item/{itemId}")
+	@ResponseBody
+	public TbItem getItemById(@PathVariable Long itemId) {
+		TbItem tbItem = itemService.getItemById(itemId);
+		return tbItem;
+	}
+			
 	@RequestMapping("/item/list")
 	@ResponseBody
 	public EUDataGridResult getItemList(Integer page, Integer rows){
