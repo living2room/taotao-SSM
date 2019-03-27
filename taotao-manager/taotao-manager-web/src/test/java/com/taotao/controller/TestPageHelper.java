adb62511dd80059ec7374b7bca75047439650814
lhy157 <965463454@qq.com>
2019-03-27 19:08:26 +0800

    LHY taotao.com

---
+++ taotao-manager/taotao-manager-web/src/test/java/com/taotao/controller/TestPageHelper.java
@@ -0,0 +1,36 @@
+package com.taotao.controller;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
+
+public class TestPageHelper {
+
+	@Test
+	public void testPageHelper() {
+        ApplicationContext applicationcontext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-*.xml");
+        
+        TbItemMapper mapper = applicationcontext.getBean(TbItemMapper.class);
+        
+        TbItemExample example =new TbItemExample();
+        
+        mapper.selectByExample(example);
+        
+        PageHelper.startPage(1, 10);
+        List<TbItem> list = mapper.selectByExample(example);
+        for (TbItem tbItem : list) {
+			System.out.println(tbItem.getTitle());
+		}
+        PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+        long total = pageInfo.getTotal();
+        System.out.println("共有商品："+total);
+	}
+}
